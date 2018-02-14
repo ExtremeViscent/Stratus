f982e9985763f17fb9894742a5eafc084b75d084
张懿麒 <zhangyiqi55732@sina.com>
2018-02-14 13:31:30 +0800

    Proto I

--- app/src/main/java/top/wyqnh/stratus/DataRecorder.java
+++ app/src/main/java/top/wyqnh/stratus/DataRecorder.java
@@ -1,22 +1,35 @@
 package top.wyqnh.stratus;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 public class DataRecorder extends Service implements SensorEventListener {
     private SensorManager mSensorManager;
     public Sensor mPressure;
     public Sensor mTemperature;
     private LocationManager mLocationManager;
-    public String TAG="DR";
+    private String locateType;
+    private static final String GPS_LOCATION_NAME = android.location.LocationManager.GPS_PROVIDER;
+
     @Override
     public void onCreate() {
 
@@ -24,11 +37,36 @@ public class DataRecorder extends Service implements SensorEventListener {
         // a particular sensor.
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE) ;
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
         mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
         Log.i("DR","Created");
+LocationListener locationListener =new LocationListener() {
+    @Override
+    public void onLocationChanged(Location location) {
+        FMC.queue.put("Longitude",String.valueOf(location.getLongitude() ));
+        FMC.queue.put("Latitude",String.valueOf(location.getLatitude()));
+        FMC.queue.put("Speed",String.valueOf(location.getSpeed()));
+        FMC.queue.put("Bearing",String.valueOf(location.getBearing()));
+        FMC.queue.put("Altitude",String.valueOf(location.getAltitude()));
+    }
 
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+};
+        mLocationManager.requestLocationUpdates(locateType, 100,0,locationListener);
         SensorEventListener TemperatureListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
--- app/src/test/java/top/wyqnh/stratus/ExampleUnitTest.java
+++ app/src/test/java/top/wyqnh/stratus/ExampleUnitTest.java
@@ -1,17 +1,218 @@
-package top.wyqnh.stratus;
+package com.lzy.gpslocation;
 
-import org.junit.Test;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import static org.junit.Assert.*;
+import com.fastaccess.permission.base.PermissionHelper;
+import com.fastaccess.permission.base.callback.OnPermissionCallback;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements OnPermissionCallback {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String GPS_LOCATION_NAME = android.location.LocationManager.GPS_PROVIDER;
+    private static final int REQUEST_PRESSMION_CODE = 10000;
+    private final static String[] MULTI_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+
+    private LocationManager locationManager;
+    private boolean isGpsEnabled;
+    private String locateType;
+    private TextView textLocationShow;
+    private Button btnLocation;
+
+    //权限检测类
+    private PermissionHelper mPermissionHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initViews();
+    }
+
+    /**
+     * 方法描述：初始化定位相关数据
+     */
+    private void initData() {
+        //获取定位服务
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        //判断是否开启GPS定位功能
+        isGpsEnabled = locationManager.isProviderEnabled(GPS_LOCATION_NAME);
+        //定位类型：GPS
+        locateType = locationManager.GPS_PROVIDER;
+        //初始化PermissionHelper
+        mPermissionHelper = PermissionHelper.getInstance(MainActivity.this);
+    }
+
+    /**
+     * 方法描述：初始化View组件信息及相关点击事件
+     */
+    private void initViews() {
+        textLocationShow = (TextView) findViewById(R.id.text_location_show);
+        btnLocation = (Button) findViewById(R.id.btn_location);
+
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+
+        ((Button)findViewById(R.id.btn_skip)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            mPermissionHelper.request(MULTI_PERMISSIONS);
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locateType); // 通过GPS获取位置
+        if (location != null) {
+            updateUI(location);
+        }
+        // 设置监听*器，自动更新的最小时间为间隔N秒(1秒为1*1000，这样写主要为了方便)或最小位移变化超过N米
+        locationManager.requestLocationUpdates(locateType, 100,0,
+                locationListener);
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        /**
+         * 位置信息变化时触发:当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+         * @param location
+         */
+        @Override
+        public void onLocationChanged(Location location) {
+            Toast.makeText(MainActivity.this, "onLocationChanged函数被触发！", Toast.LENGTH_SHORT).show();
+            updateUI(location);
+            Log.i(TAG, "时间：" + location.getTime());
+            Log.i(TAG, "经度：" + location.getLongitude());
+            Log.i(TAG, "纬度：" + location.getLatitude());
+            Log.i(TAG, "海拔：" + location.getAltitude());
+        }
+
+        /**
+         * GPS状态变化时触发:Provider被disable时触发此函数，比如GPS被关闭
+         * @param provider
+         * @param status
+         * @param extras
+         */
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                //GPS状态为可见时
+                case LocationProvider.AVAILABLE:
+                    Toast.makeText(MainActivity.this, "onStatusChanged：当前GPS状态为可见状态", Toast.LENGTH_SHORT).show();
+                    break;
+                //GPS状态为服务区外时
+                case LocationProvider.OUT_OF_SERVICE:
+                    Toast.makeText(MainActivity.this, "onStatusChanged:当前GPS状态为服务区外状态", Toast.LENGTH_SHORT).show();
+                    break;
+                //GPS状态为暂停服务时
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Toast.makeText(MainActivity.this, "onStatusChanged:当前GPS状态为暂停服务状态", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+        /**
+         * 方法描述：GPS开启时触发
+         * @param provider
+         */
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(MainActivity.this, "onProviderEnabled:方法被触发", Toast.LENGTH_SHORT).show();
+            getLocation();
+        }
+
+        /**
+         * 方法描述： GPS禁用时触发
+         * @param provider
+         */
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    /**
+     * 方法描述：在View上更新位置信息的显示
+     *
+     * @param location
+     */
+    private void updateUI(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        textLocationShow.setText("当前经度：" + longitude + "\n当前纬度：" + latitude);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPermissionHelper.onActivityForResult(requestCode);
+    }
+
+    @Override
+    public void onPermissionGranted(@NonNull String[] permissionName) {
+        getLocation();
+        Log.i("onPermissionGranted", "Permission(s) " + Arrays.toString(permissionName) + " Granted");
+    }
+
+    @Override
+    public void onPermissionDeclined(@NonNull String[] permissionName) {
+
+    }
+
+    @Override
+    public void onPermissionPreGranted(@NonNull String permissionsName) {
+
+    }
+
+    @Override
+    public void onPermissionNeedExplanation(@NonNull String permissionName) {
+
+    }
+
+    @Override
+    public void onPermissionReallyDeclined(@NonNull String permissionName) {
+
+    }
+
+    @Override
+    public void onNoPermissionNeeded() {
+
     }
 }
